4b6c467c4ef4040cfaede8a9bdd409533c43e04e
MadalinAndreiComan <comanandreimadalin@gmail.com>
2024-10-25 12:09:19 +0300

    Added puts to io

---
+++ src/io/puts.c
@@ -0,0 +1,17 @@
+#include "stdio.h"
+#include <string.h>
+#include <internal/io.h>
+
+int puts(const char *s) {
+	ssize_t size = strlen(s);
+
+	while (size > 0){
+		ssize_t written_size = write(1, s, size);
+		size -= written_size;
+		s += written_size;
+	}
+
+	write(1, "\n", 1);
+
+	return 0;
+}
